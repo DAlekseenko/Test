c14ad19b64d98edbd2b9cc3e072bb6ac9a5d7135
dalekseenko <dalekseenko88@gmail.com>
2020-05-26 12:29:44 +0300

    change script.sh

--- script.sh
+++ script.sh
@@ -3,10 +3,11 @@
 # Start the first process
 set -m
 
-# Start the primary process and put it in the background
+# Start the helper process and put it in the background
 ./chromedriver &
 
-# Start the helper process
+# Start the main process
 npm run start
 
+# Exit after working
 exit 1
